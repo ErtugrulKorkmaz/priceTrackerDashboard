d5adff2b1a6751b2926480dc943c2435b6fdb3cb
Ertugrul Korkmaz <91287241+ErtugrulKorkmaz@users.noreply.github.com>
2025-06-10 23:19:50 +0300

    Update app.py

--- app.py
+++ app.py
@@ -1,25 +1,22 @@
-# app.py
 
-from flask import Flask, flash, get_flashed_messages # <-- BURAYI GÜNCELLE
+from flask import Flask, flash, get_flashed_messages 
 from flask_sqlalchemy import SQLAlchemy
 from config import Config
 import logging
 
-# Uygulama başlatma
-app = Flask(__name__)
-app.config.from_object(Config) # config.py dosyasındaki ayarları yükle
-app.config['SECRET_KEY'] = 'senin_cok_gizli_anahtarin' # <-- YENİ EKLENECEK SATIR - Burayı güçlü ve benzersiz bir şeyle değiştir!
 
-# Veritabanı bağlantısı
+app = Flask(__name__)
+app.config.from_object(Config) 
+app.config['SECRET_KEY'] = 'senin_cok_gizli_anahtarin' 
 db = SQLAlchemy(app)
 
 # Loglama ayarları
 logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(levelname)s - %(message)s')
 
-# routes.py dosyasındaki rotaları import et (uygulama başlatıldığında yüklenirler)
+
 from routes import *
 
-# Veritabanı tablolarını oluştur (sadece ilk çalıştırmada veya tablo yoksa)
+
 with app.app_context():
     db.create_all()
     app.logger.info("Veritabanı tabloları oluşturuldu veya zaten mevcut.")
